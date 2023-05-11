d99c8596e0ad793fd883a66788a44813a62624c8
Gamer-Guy12 <123429926+Gamer-Guy12@users.noreply.github.com>
2023-05-10 17:53:17 -0700

    3 files modified

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import * as MATH from 'math.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -7,7 +8,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-alert('hi');
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -21,30 +22,23 @@ camera.position.z = 5;
 
 var forward = true;
 var count = 30;
-var vector = new THREE.Vector3();
+var forward = new THREE.Vector4();
+forward = THREE.Vector4(1, 0, 0, 0);
 var matrix = new THREE.Matrix4();
+var identity = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+matrix.set(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
 
 document.onkeydown = function (e) {
   if (e.which == 87) {
-    camera.getWorldDirection(vector);
-    camera.translateX(vector.x);
-    camera.translateY(vector.y);
-    camera.translateZ(vector.z);
+    camera.translateX(forward.x);
+    camera.translateX(forward.y);
+    camera.translateX(forward.z);
   } else if (e.which == 83) {
-    camera.getWorldDirection(vector);
-    camera.translateX(-vector.x);
-    camera.translateY(-vector.y);
-    camera.translateZ(-vector.z);
+    camera.translateX(-forward.x);
+    camera.translateX(-forward.y);
+    camera.translateX(-forward.z);
   } else if (e.which == 65) {
-    const quaternion = new THREE.Quaternion();
-    quaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI / 2);
-
-    camera.applyQuaternion(quaternion);
   } else if (e.which == 68) {
-    const quaternion = new THREE.Quaternion();
-    quaternion.setFromAxisAngle(new THREE.Vector3(-1, 0, 0), Math.PI / 2);
-
-    camera.applyQuaternion(quaternion);
   }
 };
 
